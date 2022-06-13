a7fb718cd3196694f53aaa619a01a1b3a7b13b67
Emanuel Costa Conrado <dev.conradoemanuelcosta@gmail.com>
2022-06-13 17:19:48 -0300

    Arquivos para Download

---
+++ arquivos-para-download/Download/scripts/9.2.Principais Funções.R
@@ -0,0 +1,26 @@
+esoph
+#default 6 linhas
+head(esoph)
+tail(esoph)
+#10 linhas
+head(esoph, n=10)
+
+dim(esoph)
+
+#se usar com objeto bi dimensional vai retornar colunas
+length(islands )
+
+
+colnames(esoph)
+
+
+rownames(esoph)
+
+summary(esoph)
+
+
+x = file.choose()
+x
+
+
+
